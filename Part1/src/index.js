5aa85eb6d2cea36eefc655928185499ce58e5e67
Katrin <katrin.krotova@mail.ru>
2021-06-22 18:56:02 +0300

    Доделана 1 часть. Появились звук и стили

--- Part1/src/index.js
+++ Part1/src/index.js
@@ -2,8 +2,7 @@ import { diffDates, diffToHtml } from "./datecalc.js"; // 1
 import { formatError } from "./utils.js"; // 2
 import { changeHTML } from "./select.js";
 import { remainTime } from "./timer.js";
-import { DateTime } from "./luxon.js";
-import { Howl } from "./howler.js";
+import "./howler.core.js";
 
 // Калькулятор дат
 const dateCalcForm = document.getElementById("datecalc");
@@ -25,50 +24,40 @@ function handleCalcDates(event) {
 
 // Таймер
 const timerForm = document.getElementById("timer");
-const timeLast = document.getElementById("timeLast");
 const timeError = document.getElementById("time__error");
 const pauseButton = document.getElementById("pause");
 const resetButton = document.getElementById("reset");
 
-const sound = Howl({
-    src: ['./media/alarm.webm', './media/alarm.mp3']
+const sound = new Howl({
+    src: ['../media/alarm.mp3']
 });
 
 timerForm.addEventListener('submit', function(event){
     event.preventDefault();
     timeError.innerHTML = "";
+
     let { timerSetHour, timerSetMin, timerSetSec } = event.target.elements;
     timerSetHour = timerSetHour.value, timerSetMin = timerSetMin.value, timerSetSec = timerSetSec.value;
+
     if (timerSetSec !== '0' || timerSetMin !== '0' || timerSetHour !== '0') {
         const intervalTimer = window.setInterval(myCallback, 1000, timerSetHour, timerSetMin, timerSetSec);
+        
         function myCallback() {
-            // let { remainHour, remainMin, remainSec } = remainTime(timerSetHour, timerSetMin, timerSetSec);
             let lastTime = remainTime(timerSetHour, timerSetMin, timerSetSec);
-            console.log(lastTime);
             timerForm.elements.timerSetHour.value = timerSetHour = lastTime.hours;
             timerForm.elements.timerSetMin.value = timerSetMin = lastTime.minutes;
             timerForm.elements.timerSetSec.value = timerSetSec = lastTime.seconds;
             
-            // timerSetHour = lastTime.hours, timerSetMin = lastTime.minutes, timerSetSec = lastTime.seconds;
-            
             if (lastTime.hours === 0 && lastTime.minutes === 0 && lastTime.seconds === 0) {
                 clearInterval(intervalTimer);
                 sound.play();
-                //звонок!
             }
-            // pauseButton.addEventListener('click', clearInterval(intervalTimer));
-            // resetButton.addEventListener('click', clearInterval(intervalTimer));
             pauseButton.onclick = resetButton.onclick = function() {
                 clearInterval(intervalTimer);
             };
         }
     } else timeError.innerHTML = formatError("Для запуска таймера необходимо установить время");
-
-    // pauseButton.addEventListener('click', clearInterval(intervalTimer));
-    // resetButton.addEventListener('click', clearInterval(intervalTimer));
-    // pauseButton.onclick = resetButton.onclick = clearInterval(intervalTimer);
 });
-
 
 //Перключение между функциями
 const pageDate = document.querySelector(".pageDates");
